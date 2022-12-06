e4156e841b1c5663406c7ed7f755ee33bae4bc19
Antoine <antoine.lutfalla@gmail.com>
2022-12-06 11:06:19 +0100

    webskte structuration

--- main.py
+++ main.py
@@ -2,6 +2,19 @@
 
 The file 'main.py' was made to run the website and import libraries.
 
+npm install react-scripts
+npm install node-sass
+npm install react-router-dom
+npm install @emailjs/browser
+npm install @fortawesome/free-brands-svg-icons
+npm install @fortawesome/free-solid-svg-icons
+npm install @fortawesome/react-fontawesome
+npm install animate.css
+npm install gsap-trial
+npm install loaders.css
+npm install react-leaflet
+npm install react-loaders
+
 '''
 
 # This part is going to import the librairies and the dependencies.
--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -5,49 +5,15 @@ import Layout from "./components/Layout/Layout";
 import { Routes, BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
-	// usestate for setting a javascript
-	// object for storing and using data
-	const [homePage, setdata] = useState({
-		name: "",
-		age: 0,
-		date: "",
-		programming: "",
-	});
-
-	// Using useEffect for single rendering
-	useEffect(() => {
-		// Using fetch to fetch the api from
-		// flask server it will be redirected to proxy
-		fetch("/data").then((res) =>
-			res.json().then((homePage) => {
-				// Setting a data from api
-				setdata({
-					name: homePage.Name,
-					age: homePage.Age,
-					date: homePage.Date,
-					programming: homePage.programming,
-				});
-			})
-		);
-	}, []);
-
 	return (
-		<div className="App">
+		<>
 			<BrowserRouter>
         		<Routes>
-					<header className="App-header">
-						<Route path="/" element={<Layout />} />
-						<h1>My portfolio !</h1>
-						<p>{homePage.name}</p>
-						<p>{homePage.age}</p>
-						<p>{homePage.date}</p>
-						<p>{homePage.programming}</p>
-
-					</header>
+					<Route path="/" element={<Layout />} />
 				</Routes>
 			</BrowserRouter>
-		</div>
-	);
+		</>
+	)
 }
 
 export default App;

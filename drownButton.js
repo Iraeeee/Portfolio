418c3af41e61430d2f1f10e0d1c6e9b2584cde53
Iraeeee <antoine.lutfalla@gmail.com>
2023-01-19 23:56:31 -0800

    waveButton fix

--- drownButton.js
+++ drownButton.js
@@ -3,7 +3,8 @@ function createWaterWave() {
   const waveButton = document.getElementById("create-wave-button");
   const waterWave = document.getElementById("water-wave")
   waveButton.style.animation = "waveUp 3s linear";
-  waterWave.style.animation = "waveUp 3s linear"
+  waterWave.style.animation = "waveUp 3s linear";
+  setTimeout(() => {waveButton.style.display = "none"}, 3000)
 
   increaseSize();
 

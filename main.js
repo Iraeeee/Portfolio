86ac68c9b1b322f411eaf073ce5971b81de55bef
Iraeeee <antoine.lutfalla@gmail.com>
2023-02-27 12:57:40 +0100

    delay fix

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 function createWaterWave() {
 
   const waveButton = document.getElementById("create-wave-button");
-  const waterWave = document.getElementById("water-wave")
+  const waterWave = document.getElementById("water-wave");
 
   waveButton.style.animation = "waveUp 3s linear";
-  waterWave.style.animation = "waveUp 3s linear";
 
   setSizeTo("blue-strip", "1360px");
 
-  setTimeout(() => {waveButton.style.display = "none"}, 3000)
-  setTimeout(() => {window.location.href = 'homePage/index.html'}, 2000)
+  setTimeout(() => {
+    waterWave.style.animation = "waveUp 3s linear";
+  }, 15);
+
+  setTimeout(() => {
+    waveButton.style.display = "none";
+    window.location.href = 'homePage/index.html';
+  }, 3000);
 }
 
 function setSizeTo(arg, height) {
